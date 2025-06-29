cdb3746c11e0cc7f802f172706cbd5259f60e470
SeulGi0117 <jusanha17@gmail.com>
2025-06-29 13:34:29 +0900

    fix: main.dart import 및 MaterialApp builder 오류 해결, 상세기록/다크모드 안정화

--- cow_management/lib/main.dart
+++ cow_management/lib/main.dart
@@ -8,87 +8,8 @@ import 'providers/theme_provider.dart';
 import 'screens/splash_screen.dart';
 import 'screens/onboarding/onboarding_page.dart';
 import 'screens/ai_chatbot/app_wrapper.dart';
-import 'utils/error_utils.dart';
 
-import 'package:logging/logging.dart';
-import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/foundation.dart';
-
-import 'package:cow_management/models/cow.dart';
-import 'package:cow_management/models/Detail/feeding_record.dart';
-import 'package:cow_management/models/Detail/Health/vaccination_record.dart';
-import 'package:cow_management/models/Detail/Health/treatment_record_model.dart';
-import 'package:cow_management/models/Detail/Health/weight_record_model.dart';
-
-import 'package:cow_management/providers/DetailPage/breeding_record_provider.dart';
-import 'package:cow_management/providers/DetailPage/milking_record_provider.dart';
-import 'package:cow_management/providers/DetailPage/Health/health_check_provider.dart';
-import 'package:cow_management/providers/DetailPage/Health/vaccination_record_provider.dart';
-import 'package:cow_management/providers/DetailPage/feeding_record_provider.dart';
-import 'package:cow_management/providers/DetailPage/Health/weight_record_provider.dart';
-import 'package:cow_management/providers/DetailPage/Health/treatment_record_provider.dart';
-import 'package:cow_management/providers/DetailPage/Reproduction/estrus_record_provider.dart';
-import 'package:cow_management/providers/DetailPage/Reproduction/insemination_record_provider.dart';
-import 'package:cow_management/providers/DetailPage/Reproduction/pregnancy_check_provider.dart';
-import 'package:cow_management/providers/DetailPage/Reproduction/calving_record_provider.dart';
-
-import 'package:cow_management/screens/ai_analysis/analysis_page.dart';
-import 'package:cow_management/screens/ai_chatbot/chatbot_history_page.dart';
-
-import 'package:cow_management/screens/accounts/login.dart';
-import 'package:cow_management/screens/accounts/signup.dart';
-import 'package:cow_management/screens/onboarding/auth_selection_page.dart';
-
-import 'package:cow_management/screens/home/home_page.dart';
-
-import 'package:cow_management/screens/profile/profile_page.dart';
-
-import 'package:cow_management/screens/cow_list/cow_list_page.dart';
-import 'package:cow_management/screens/cow_list/cow_detail_page.dart';
-import 'package:cow_management/screens/cow_list/cow_edit_page.dart';
-
-import 'package:cow_management/screens/cow_list/Cow_Detail/Milk/milk_add_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Milk/milk_list_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Milk/milk_detail_page.dart';
-
-import 'package:cow_management/screens/cow_list/Cow_Detail/Breeding/breeding_list_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Breeding/breeding_detail_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Breeding/breeding_add_page.dart';
-
-import 'package:cow_management/screens/cow_list/Cow_Detail/Health/health_check_add_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Health/health_check_detail_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Health/health_check_list_page.dart';
-
-import 'package:cow_management/screens/cow_list/Cow_Detail/Vaccination/vaccination_add_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Vaccination/vaccination_detail_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Vaccination/vaccination_list_page.dart';
-
-import 'package:cow_management/screens/cow_list/Cow_Detail/Feeding/feeding_add_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Feeding/feeding_list_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Feeding/feeding_detail_page.dart';
-
-import 'package:cow_management/screens/splash_screen.dart';
-
-import 'package:cow_management/screens/cow_list/Cow_Detail/Weight/weight_add_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Weight/weight_list_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Weight/weight_detail_page.dart';
-
-import 'package:cow_management/screens/cow_list/Cow_Detail/Treatment/treatment_add_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Treatment/treatment_list_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Treatment/treatment_detail_page..dart';
-
-import 'package:cow_management/screens/cow_list/Cow_Detail/Estrus/estrus_add_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Estrus/estrus_list_page.dart';
-
-import 'package:cow_management/screens/cow_list/Cow_Detail/Insemination/insemination_list_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Insemination/insemination_add_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Pregnancy/pregnancy_check_list_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Pregnancy/pregnancy_check_add_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Calving/calving_record_list_page.dart';
-import 'package:cow_management/screens/cow_list/Cow_Detail/Calving/calving_record_add_page.dart';
-
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   
@@ -107,7 +28,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => ThemeProvider()),
       ],
       child: Consumer<ThemeProvider>(
-        builder: (context, themeProvider, child) {
+        builder: (context, themeProvider, _) {
           return MaterialApp(
             title: 'BlackCows 젖소 관리',
             debugShowCheckedModeBanner: false,
@@ -202,8 +123,8 @@ class MyApp extends StatelessWidget {
             },
             builder: (context, child) {
               return MediaQuery(
-                data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
-                child: child!,
+                data: MediaQuery.of(context).copyWith(textScaler: const TextScaler.linear(1.0)),
+                child: child ?? const SizedBox.shrink(),
               );
             },
           );
@@ -211,5 +132,4 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
-}
-
+} 
