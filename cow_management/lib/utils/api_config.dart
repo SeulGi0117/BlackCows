9e4240a6e973b2fb7a847de8aa576dd5f7e4dd1f
BogyunJeong <ototkk58@naver.com>
2025-07-14 00:23:17 +0900

    Update api_config.dart

--- cow_management/lib/utils/api_config.dart
+++ cow_management/lib/utils/api_config.dart
@@ -4,7 +4,7 @@ class ApiConfig {
   static String get baseUrl {
     // dart-define으로 전달된 API_BASE_URL 우선 사용
     return const String.fromEnvironment('API_BASE_URL',
-        defaultValue: 'http://192.168.0.8:8000' // 웹/모바일 공통 기본값
+        defaultValue: 'https://api.blackcowsdairy.com'  // 웹/모바일 공통 기본값
         );
   }
 }
